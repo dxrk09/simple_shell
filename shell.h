2a09e1be0f6a40476b1d4a7e039e1969b93c2960
Dxrk <118543407+dxrk09@users.noreply.github.com>
2023-11-07 14:42:42 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -20,13 +20,13 @@ char **tokenizer(char *line);
 int _execute(char **command, char **argv, int idx);
 char *_getenv(char *variable);
 char *_getpath(path *command);
-
+char *_itoa(int n);
 char *_strdup(const char *str);
 int _strcmp(char *s1, char *s2);
 int _strlen(char *s);
 char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
-
+void reverse_string(char *str, int len);
 void freearray2D(char **array);
 void print_error(char *name, char *cmd, int idx);
 
